a873f87525e19d7e0ef8e6384f759490619066a3
Alex Supkay <asupkay1124@gmail.com>
2017-11-28 18:01:54 -0500

    Stop tracking bad exchanges and switch to bid/ask format, grab averages

--- vectors/js/index.js
+++ vectors/js/index.js
@@ -1,25 +1,98 @@
 var https = require("https");
 
 module.exports = {
-	getBitcoinPrices: (req, res) => {
-        getCoinMarketCapPrice().then((coinMarketCapPrice) => {
-            getCoinBasePrice().then((coinBasePrice) => {
-                res.status(200).json({
-                    confirmation: 'success',
-                    btcAverage: coinMarketCapPrice,
-                    coinBaseBuy: coinBasePrice
-                });
-            }).catch((error) => {
 
+
+	getBitcoinPrices: (req, res) => {
+        getCexIOPrice().then((cexIOInfo) => {
+            let bids = {};
+            let asks = {};
+
+            bids.cexio = cexIOInfo.bid;
+            asks.cexio = cexIOInfo.ask;
+
+            let statInfo = resolveInformation(bids, asks);
+
+            res.status(200).json({
+                confirmation: 'success',
+                averageBid: statInfo.averageBid,
+                highestBidEx: statInfo.highestBidExchange,
+                bid: bids,
+                averageAsk: statInfo.averageAsk,
+                lowestAskEx: statInfo.lowestAskExchange,
+                ask: asks
             });
         }).catch((error) => {
             res.status(503).json({
                 confirmation: 'failure',
-                message: error
+                message: error.message
             });
         });   
 	}
 
+}
+
+function resolveInformation(bids, asks) {
+    let amountOfExchanges = 0;
+
+    let highestBidExchange;
+    let highestBidAmount = 0;
+
+    let lowestAskExchange;
+    let lowestAskAmount = Infinity;
+
+    let averageBid = 0;
+    let averageAsk = 0;
+
+   
+
+    for(let key in bids) {
+        amountOfExchanges++;
+        cBid = bids[key];
+
+        averageBid += cBid;
+
+        if(cBid > highestBidAmount) {
+            highestBidExchange = key;
+            highestBidAmount = cBid;
+        }
+    }
+
+    for(let key in asks) {
+        cAsk = asks[key];
+        averageAsk = asks[key]
+
+        if(cAsk < lowestAskAmount) {
+            lowestAskExchange = key;
+            lowestAskAmount = cAsk;
+        }
+    }
+
+    averageBid = averageBid/amountOfExchanges;
+    averageAsk = averageAsk/amountOfExchanges;
+
+    returnInformation = {
+        highestBidExchange: highestBidExchange,
+        lowestAskExchange: lowestAskExchange,
+        averageBid: averageBid,
+        averageAsk: averageAsk
+    }
+
+    return returnInformation;
+}
+
+function getCexIOPrice() {
+    var promise = new Promise(
+    function(resolve, reject) {
+            https.get('https://cex.io/api/ticker/BTC/USD', (res) => {
+            res.on('data', (d) => {
+                bitcoinJSONData = JSON.parse(d)
+                console.log("Cex.io:\n $j", bitcoinJSONData);
+                resolve(bitcoinJSONData);
+            });
+        });
+    });
+    return promise;
 }
 
 function getCoinMarketCapPrice() {
@@ -49,3 +122,5 @@ function getCoinBasePrice() {
     });
     return promise;
 }
+
+
