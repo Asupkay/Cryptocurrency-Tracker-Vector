0e41255733a97c023911e92f345dae639115a527
Alex Supkay <asupkay1124@gmail.com>
2017-11-29 17:50:10 -0500

    Add gdax prices

--- vectors/js/index.js
+++ vectors/js/index.js
@@ -3,31 +3,40 @@ var https = require("https");
 module.exports = {
 	getBitcoinPrices: (req, res) => {
         getCexIOPrice().then((cexIOInfo) => {
-            let exchangeBids = {};
-            let exchangeAsks = {};
+            getGdaxPrice().then((gdaxInfo) => {
+                let exchangeBids = {};
+                let exchangeAsks = {};
 
-            exchangeBids.cexio = cexIOInfo.bid;
-            exchangeAsks.cexio = cexIOInfo.ask;
+                exchangeBids.cexio = cexIOInfo.bid;
+                exchangeAsks.cexio = cexIOInfo.ask;
+                exchangeBids.gdax = gdaxInfo.bid;
+                exchangeAsks.gdax = gdaxInfo.ask;
 
-            let statInfo = resolveInformation(exchangeBids, exchangeAsks);
+                let statInfo = resolveInformation(exchangeBids, exchangeAsks);
 
-            res.status(200).json({
-                confirmation: 'success',
-                bids: {
-                    average: statInfo.averageBid,
-                    highestExchange: statInfo.highestBidExchange,
-                    exchanges: exchangeBids
-                },
-                asks: {
-                    average: statInfo.averageAsk,
-                    lowestExchange: statInfo.lowestAskExchange,
-                    exchanges: exchangeAsks
-                }
+                res.status(200).json({
+                    confirmation: 'success',
+                    bids: {
+                        average: statInfo.averageBid,
+                        highestExchange: statInfo.highestBidExchange,
+                        exchanges: exchangeBids
+                    },
+                    asks: {
+                        average: statInfo.averageAsk,
+                        lowestExchange: statInfo.lowestAskExchange,
+                        exchanges: exchangeAsks
+                    }
+                });
+            }).catch((error) => {
+                res.status(500).json({
+                    confirmation: 'failure',
+                    message: "Gdax error" + error.message
+                });
             });
         }).catch((error) => {
             res.status(503).json({
                 confirmation: 'failure',
-                message: error.message
+                message: "Cexio error" + error.message
             });
         });   
 	}
@@ -62,7 +71,8 @@ function resolveInformation(bids, asks) {
 
     for(let key in asks) {
         cAsk = asks[key];
-        averageAsk = asks[key]
+
+        averageAsk += asks[key]
 
         if(cAsk < lowestAskAmount) {
             lowestAskExchange = key;
@@ -84,12 +94,37 @@ function resolveInformation(bids, asks) {
 }
 
 function getCexIOPrice() {
-    var promise = new Promise(
+    let promise = new Promise(
     function(resolve, reject) {
             https.get('https://cex.io/api/ticker/BTC/USD', (res) => {
             res.on('data', (d) => {
                 bitcoinJSONData = JSON.parse(d)
                 console.log("Cex.io:\n $j", bitcoinJSONData);
+                resolve(bitcoinJSONData);
+            });
+        });
+    });
+    return promise;
+}
+
+function getGdaxPrice() {
+    let promise = new Promise(
+    function(resolve, reject) {
+            
+            const option = {
+                hostname: 'api.gdax.com',
+                path: '/products/BTC-USD/ticker',
+                headers: {
+                    'User-Agent': 'Node-JS Exchange Checker'
+                }
+            };
+
+            https.get(option, (res) => {
+            res.on('data', (d) => {
+                bitcoinJSONData = JSON.parse(d)
+                console.log("Gdax.com:\n $j", bitcoinJSONData);
+                bitcoinJSONData.bid = parseFloat(bitcoinJSONData.bid);
+                bitcoinJSONData.ask = parseFloat(bitcoinJSONData.ask);
                 resolve(bitcoinJSONData);
             });
         });
